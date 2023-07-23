fbd1a347a35f329cca4cb743cb24081b78c94488
hikohiko22 <1192.2960.kawakura@gmail.com>
2023-07-23 19:04:20 +0900

    最新順に表示する

--- src/components/DisplayTweets.js
+++ src/components/DisplayTweets.js
@@ -1,18 +1,18 @@
-// DisplayTweets.js
 import React, { useEffect, useState } from 'react';
 import { database, ref } from '../firebase';
-import { onValue, query, limitToFirst } from 'firebase/database';
+import { onValue, query, orderByChild, limitToLast } from 'firebase/database';
 
 const DisplayTweets = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const dbRef = ref(database, 'tweets');
-    const q = query(dbRef, limitToFirst(100));
+    const q = query(dbRef, orderByChild('Tweet date timestamp'), limitToLast(100));
     const unsub = onValue(q, (snapshot) => {
       const fbData = snapshot.val();
-      const newDataArray = Object.entries(fbData).map(([id, value]) => ({id, ...value}));
-      setData(newDataArray);
+      const newDataArray = Object.entries(fbData).map(([id, value]) => ({ id, ...value }));
+      const sortedDataArray = newDataArray.sort((a, b) => b['Tweet date timestamp'] - a['Tweet date timestamp']);
+      setData(sortedDataArray);
     });
 
     // Clean-up function
@@ -28,8 +28,11 @@ const DisplayTweets = () => {
         <div key={item.id}>
           <h2>Tweet ID: {item.id}</h2>
           <p>Display name: {item['Display name']}</p>
-          <p>Tweet content: {item['Tweet content']}</p>
+          <p>Tweet Date: {item['Tweet date']}</p>
+          <p>Tweet Date timestamp: {item['Tweet date timestamp']}</p>
+          <p>Tweet content: {item['Tweet content'].replace(/ https:\/\/t\.co\/\S*/, "")}</p>
           <p>Tweet URL: <a href={item['Tweet URL']} target="_blank" rel="noopener noreferrer">Link</a></p>
+          <img src={item['Media URL']} alt="Tweet Media" />
           <p>Likes: {item.Likes}</p>
           {/* 他のフィールドも同様に表示できます */}
         </div>
