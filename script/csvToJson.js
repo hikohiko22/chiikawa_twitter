39af01a42294c1d3560110b47a9152a21abe04f6
hikohiko22 <1192.2960.kawakura@gmail.com>
2023-07-23 18:40:59 +0900

    add timestamp

--- script/csvToJson.js
+++ script/csvToJson.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const csv = require('csv-parser');
 const { v4: uuidv4 } = require('uuid');
 
-const csvFilePath = './script/tweets/ngnchiikawa/allTweets.csv'; // CSVファイルのパス
+const csvFilePath = './script/tweets/ngnchiikawa.csv'; // CSVファイルのパス
 const jsonFilePath = './script/json/ngnchiikawa.json'; // JSONファイルのパス
 
 let jsonData = {};
@@ -10,8 +10,9 @@ let jsonData = {};
 fs.createReadStream(csvFilePath)
   .pipe(csv())
   .on('data', (row) => {
-    let key = uuidv4();
-    jsonData[key] = row;
+    let tweetDate = new Date(row['Tweet date']).getTime(); // Tweet dateをUNIXタイムスタンプに変換
+    let key = uuidv4(); // UUIDを生成してキーとして使用
+    jsonData[key] = { ...row, 'Tweet date timestamp': tweetDate };
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
