e866fe33aa1fa564741903dabbfb05e42b1a497c
hikohiko22 <1192.2960.kawakura@gmail.com>
2023-07-25 16:59:51 +0900

    生成するjsonの形式を変更 * key値をtweetのstatusにする * mediaURLのリクエストパラメータを消す

--- script/csvToJson.js
+++ script/csvToJson.js
@@ -5,14 +5,22 @@ const { v4: uuidv4 } = require('uuid');
 const csvFilePath = './script/tweets/ngnchiikawa.csv'; // CSVファイルのパス
 const jsonFilePath = './script/json/ngnchiikawa.json'; // JSONファイルのパス
 
+function removeQueryParam(url) {
+  const urlObj = new URL(url);
+  urlObj.searchParams.delete('name');
+  return urlObj.toString();
+}
+
 let jsonData = {};
 
 fs.createReadStream(csvFilePath)
   .pipe(csv())
   .on('data', (row) => {
     let tweetDate = new Date(row['Tweet date']).getTime(); // Tweet dateをUNIXタイムスタンプに変換
-    let key = uuidv4(); // UUIDを生成してキーとして使用
-    jsonData[key] = { ...row, 'Tweet date timestamp': tweetDate };
+    const statusId = row['Tweet URL'].match(/status\/(\d+)/); // Tweet URLからstatus以下の数字の羅列を抽出
+    const key = statusId ? statusId[1] : uuidv4(); // 抽出したstatusIdがあればそれをキーとして使用し、なければUUIDを生成してキーとする
+    const mediaURL = row['Media URL'] ? removeQueryParam(row['Media URL']) : '';
+    jsonData[key] = { ...row, 'Tweet date timestamp': tweetDate, 'Media URL': mediaURL };
   })
   .on('end', () => {
     console.log('CSV file successfully processed');
